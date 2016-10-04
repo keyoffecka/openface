ec56fd4acd60f027011616178b5d9d38cab94ef2
Konstantin Golub <key.offecka@runbox.com>
2016-10-03 19:04:51 -0500

    RPC-1597: Result object formatting

--- demos/web/websocket-server.py
+++ demos/web/websocket-server.py
@@ -117,7 +117,7 @@ class OpenFaceServerProtocol(WebSocketServerProtocol):
             self.loadState(msg['images'], msg['training'], msg['people'])
         elif msg['type'] == "COMPARE":
             d = getRep(msg['img1']) - getRep(msg['img2'])
-            self.sendMessage('{"distance": {:0.3f}}'.format(np.dot(d, d)))
+            self.sendMessage('{"distance": ' + np.dot(d, d) + '}')
         elif msg['type'] == "NULL":
             self.sendMessage('{"type": "NULL"}')
         elif msg['type'] == "FRAME":
